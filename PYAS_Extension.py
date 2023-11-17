8baae2f446bba8ef3d535b5deca330ac3c527623
oooalanyan <131363484+oooalanyan@users.noreply.github.com>
2023-11-17 21:54:10 +0800

    Add files via upload

---
+++ PYAS_Extension.py
@@ -0,0 +1,10 @@
+slist = [".exe",".dll",".com",".msi",".scr",
+         ".bat",".cmd",".ps1",".vbs",".jar",
+         ".js",".elf",".doc",".xls",".xlsx"]
+
+alist = [".ppt",".pptx",".wps",".txt",".rtf",
+         ".pdf",".xls",".xlsx",".doc",".docx",
+         ".jpg",".jpeg",".png",".webp",".gif",
+         ".mp3",".wav",".aac",".ogg",".flac",
+         ".mp4",".avi",".mov",".wmv",".mkv",
+         ".zip",".7z",".rar",".tar",".gz"]
